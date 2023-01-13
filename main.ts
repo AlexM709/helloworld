004801836e730f57a3c23df7e11d211962343976
AlexM709 <122555304+AlexM709@users.noreply.github.com>
2023-01-13 12:06:12 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,4 +4,4 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.House)
 })
-basic.showIcon(IconNames.Square)
+basic.showIcon(IconNames.Yes)
