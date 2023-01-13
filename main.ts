076a830ca88858c1b3c7d7d2d42acb47ed813e01
AlexM709 <122555304+AlexM709@users.noreply.github.com>
2023-01-13 12:05:23 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.LeftTriangle)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.House)
+})
+basic.showIcon(IconNames.Square)
